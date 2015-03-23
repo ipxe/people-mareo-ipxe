mgenov/ipxe

[lldp] Implementation of the LLDP protocol
The LLDP setting space can be accessed once the first LLDP packet is received.

General syntax is: netX.lldp/<offset>.<size>.<count>.<type>
The fetched value being the <count>-th TLV of type <type>. <offset> and
<size> allow the user to get specific parts of the answer in order to parse
TLV type unknown to iPXE, a <size> of zero means "until the end".

The predefined settings are:
  - [0.0.0.1] chassisid  - Chassis ID
  - [0.0.0.2] portid     - Port ID
  - [0.0.0.4] portdesc   - Port description
  - [0.0.0.5] systemname - System name
  - [0.0.0.6] systemdesc - System description

  - [X.X.X.X] lldpsource - A special setting allowing the user to get the
                           source address of the LLDP packet
  - [X.X.X.X] lldpdest   - A special setting allowing the user to get the
                           destination address of the LLDP packet

Examples:
   iPXE> show netX.lldp/2.4.0.8:ipv4
   net0.lldp/2.4.0.8:ipv4 = 172.16.0.1

   iPXE> show netX.lldp/4 # equivalent to: show netX.lldp/0.0.0.4
   net0.lldp/portdesc:string = tap0

   iPXE> show netX.lldp/portid # equivalent to: show netX.lldp/0.0.0.2
   net0.lldp/portid:hex = 46:75:7a:43:c2:2f

   iPXE> show netX.lldp/lldpdest
   net0.lldp/lldpdest:hex = 01:80:c2:00:00:0e

   See also contrib/scripts/lldp.ipxe for a real-life example.

Signed-off-by: Marin Hannache <git@mareo.fr>

src/include/ipxe/errfile.h
@@ -226,6 +226,7 @@ FILE_LICENCE ( GPL2_OR_LATER );
 #define ERRFILE_ping			( ERRFILE_NET | 0x003a0000 )
 #define ERRFILE_dhcpv6			( ERRFILE_NET | 0x003b0000 )
 #define ERRFILE_nfs_uri			( ERRFILE_NET | 0x003c0000 )
+#define ERRFILE_lldp			( ERRFILE_NET | 0x003d0000 )
 
 #define ERRFILE_image		      ( ERRFILE_IMAGE | 0x00000000 )
 #define ERRFILE_elf		      ( ERRFILE_IMAGE | 0x00010000 )

ipxe/people-mareo-ipxe

src/include/ipxe/errfile.h
@@ -235,6 +235,7 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
 #define ERRFILE_dhcpv6			( ERRFILE_NET | 0x003b0000 )
 #define ERRFILE_nfs_uri			( ERRFILE_NET | 0x003c0000 )
 #define ERRFILE_rndis			( ERRFILE_NET | 0x003d0000 )
+#define ERRFILE_lldp			( ERRFILE_NET | 0x003e0000 )
 
 #define ERRFILE_image		      ( ERRFILE_IMAGE | 0x00000000 )
 #define ERRFILE_elf		      ( ERRFILE_IMAGE | 0x00010000 )